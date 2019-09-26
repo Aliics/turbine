b7a9d4b727d6ed22d46ba56ebf21059c1cae585e
Alexander Johnston <Aliics@hotmail.com>
2019-09-26 23:58:10 +1200

    Added serialization test

---
+++ src/conf.rs
@@ -0,0 +1,61 @@
+extern crate serde;
+extern crate toml;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    server: Server,
+    web: Web,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Server {
+    address: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Web {
+    http: String,
+    directory: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    #[test]
+    fn config_deserialize() {
+        let some_toml = given_example_conf();
+        let actual_conf = toml::from_str::<Config>(&some_toml).unwrap();
+        let expect_conf = given_conf();
+
+        assert_eq!(actual_conf.server.address, expect_conf.server.address);
+        assert_eq!(actual_conf.web.http, expect_conf.web.http);
+        assert_eq!(actual_conf.web.directory, expect_conf.web.directory);
+    }
+
+    #[test]
+    fn config_serialize() {
+        let actual_conf = toml::to_string::<Config>(&given_conf()).unwrap();
+        let expected_toml = given_example_conf();
+
+        assert_eq!(actual_conf.trim(), expected_toml.trim());
+    }
+
+    fn given_example_conf() -> String {
+        fs::read_to_string("tests/example-conf.toml").unwrap()
+    }
+
+    fn given_conf() -> Config {
+        Config {
+            server: Server {
+                address: "localhost:8080".to_string(),
+            },
+            web: Web {
+                http: "1.1".to_string(),
+                directory: "${HOME}/development/turbine/tests/http".to_string(),
+            },
+        }
+    }
+}
