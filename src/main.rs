18d7a1a51681e1c52b2abbdb9102aee2c54b6a53
Alexander Johnston <Aliics@hotmail.com>
2019-09-26 16:28:40 +1200

    Created basic http web server for static content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::{
+    format, fs,
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+    thread,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let tcp = TcpListener::bind("localhost:8080").unwrap();
+
+    for incoming in tcp.incoming() {
+        thread::spawn(|| say_hello(incoming.unwrap()));
+    }
+}
+
+fn say_hello(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    stream.read(&mut buf).unwrap();
+
+    let buf_str = String::from_utf8_lossy(&mut buf);
+    let buf_split: Vec<&str> = buf_str.split(' ').collect();
+    let page_path = format!("pages/{}", buf_split[1]);
+
+    let response_data = match fs::read_to_string(page_path) {
+        Ok(html) => {
+            let http_resp = fs::read_to_string("http/ok-html").unwrap();
+            format!("{}\r\n{}", http_resp, html)
+        }
+        Err(..) => fs::read_to_string("http/not-found").unwrap(),
+    };
+
+    stream.write(response_data.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
