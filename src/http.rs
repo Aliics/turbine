eaedbb82da584d7db4db1c6908b78fa0c47ee5ca
Alexander Johnston <Aliics@hotmail.com>
2019-09-27 13:33:12 +1200

    Added file extension gathering

--- src/http.rs
+++ src/http.rs
@@ -26,7 +26,21 @@ pub mod tcp {
     fn parse_status_line(request: String) -> (String, String, String) {
         let status_line: &str = request.split('\n').collect::<Vec<&str>>()[0];
         let pieces: Vec<&str> = status_line.split(' ').collect();
-        (pieces[0].to_string(), pieces[1].to_string(), pieces[2].to_string())
+        (
+            pieces[0].to_string(),
+            pieces[1].to_string(),
+            pieces[2].to_string(),
+        )
+    }
+
+    fn get_path_file_ext(path: String) -> String {
+        let dropped_params = match path.find("?") {
+            Some(i) => path.split_at(i).0.to_string(),
+            None => path,
+        };
+        let split_path: Vec<&str> = dropped_params.split(".").collect();
+        let last_index = split_path.len() - 1;
+        split_path[last_index].to_string()
     }
 
     #[cfg(test)]
@@ -42,6 +56,24 @@ pub mod tcp {
             assert_eq!(method, "GET");
             assert_eq!(path, "/index.html");
             assert_eq!(version, "HTTP/1.1");
+        }
+
+        #[test]
+        fn getting_path_file_ext() {
+            let path = "index.html".to_string();
+
+            let actual_ext = get_path_file_ext(path);
+
+            assert_eq!(actual_ext, "html");
+        }
+
+        #[test]
+        fn getting_complex_path_file_ext() {
+            let path = "main.css?foo=bar".to_string();
+
+            let actual_ext = get_path_file_ext(path);
+
+            assert_eq!(actual_ext, "css");
         }
     }
 }
